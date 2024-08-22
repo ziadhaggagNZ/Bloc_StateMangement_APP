b5d2e62b00becc763341d4490d3a1a25c9a8eb89
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-22 20:19:57 +0300

    BlocProvider

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breaking/business_logic/cubit/characters_cubit.dart';
+import 'package:flutter_breaking/data/repository/characters_repository.dart';
+import 'package:flutter_breaking/data/web_services/characters_web_services.dart';
 import 'package:flutter_breaking/presentation/screens/characters_details.dart';
 import 'package:flutter_breaking/presentation/screens/characters_screen.dart';
 
 import 'constants/strings.dart';
 
 class AppRouter {
-  Route? generateRoute(RouteSettings settings)
-  {
-    switch(settings.name){
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+  AppRouter() {
+    charactersRepository =
+        CharactersRepository(charactersWebServices: CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case CharactarScreen:
-      return MaterialPageRoute(builder: (_) => CharactersScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                create: (BuildContext) =>
+                    charactersCubit,
+                    child: CharactersScreen(),
+                    ));
 
       case CharactarDetailsScreen:
-      return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
-
+       return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
   }
 }
