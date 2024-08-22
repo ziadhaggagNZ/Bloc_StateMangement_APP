8a687db558332ea12314cd8d7e174ad1202169b3
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-22 20:54:07 +0300

    BlocBuilder

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breaking/business_logic/cubit/characters_cubit.dart';
+import 'package:flutter_breaking/constants/my_colors.dart';
+import 'package:flutter_breaking/data/models/characters.dart';
+import 'package:flutter_breaking/presentation/widgets/character_item.dart';
 
-class CharactersScreen extends StatelessWidget {
-  const CharactersScreen({super.key});
+class CharactersScreen extends StatefulWidget {
+  CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
+
+  Widget buildBlocWidget() {
+    return BlocBuilder<CharactersCubit, CharactersState>(
+      builder: (context, State) {
+        if (State is CharactersLoaded) {
+          allCharacters = (State).Characters;
+          return buildLoadedLostWidgets();
+        } else {
+          return showLoadingIndicator();
+        }
+      },
+    );
+  }
+
+  Widget showLoadingIndicator() {
+    return const Center(
+        child: CircularProgressIndicator(
+      color: MyColors.myYellow,
+    ));
+  }
+
+  Widget buildLoadedLostWidgets() {
+    return SingleChildScrollView(
+      child: Container(
+        color: MyColors.myGrey,
+        child: Column(
+          children: [
+            buildCharactersList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCharactersList() {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2 / 3,
+            crossAxisSpacing: 1,
+            mainAxisSpacing: 1
+            ),
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          // Todo : not done
+          return CharacterItem();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text("ziad");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColors.myYellow,
+        title: Text(
+          "characters",
+          style: TextStyle(color: MyColors.myGrey),
+        ),
+      ),
+      body: buildBlocWidget(),
+    );
   }
 }
